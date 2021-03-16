713d2630e0aa525cd3a4b6cf1810c81300bf163d
Chris Allen <Chris@zipcodes-MacBook-Pro-350.local>
2021-03-16 14:29:23 -0400

    Did work on Casino Engine

--- src/main/java/io/zipcoder/casino/CasinoEngine.java
+++ src/main/java/io/zipcoder/casino/CasinoEngine.java
@@ -1,19 +1,71 @@
 package io.zipcoder.casino;
 
-import io.zipcoder.casino.Player.Player;
+import io.zipcoder.casino.CardGame.BlackJack;
+import io.zipcoder.casino.CardGame.GoFishGame;
+import io.zipcoder.casino.GameInterface.DiceGame.CrapsGame;
+import io.zipcoder.casino.Player.*;
+import io.zipcoder.casino.utilities.Console;
 
 import javax.swing.text.PlainDocument;
+import java.io.InputStream;
+import java.util.Scanner;
 
 public class CasinoEngine {
     Player currentPlayer;
+    Console myConsole;
 
     public CasinoEngine(){
-
+        currentPlayer = new Player("Player1");
+        myConsole = new Console(System.in,System.out);
     }
 
     public void run(){
+        while(true){
+            String gameSelection = myConsole.getStringInput("Hi "+currentPlayer.getName()+"!\nWhat game would you like to play?\n1. BlackJack\n2. Craps\n3. ChoHan\n4. Go Fish\n99. Quit");
+            if (gameSelection.equals("1")){
+                runBlackJackGame(currentPlayer);
+            }
+            else if(gameSelection.equals("2")){
+                runCraps(currentPlayer);
+            }
+            else if(gameSelection.equals("3")){
+                runChoHan(currentPlayer);
+            }
+            else if(gameSelection.equals("4")){
+                runGoFish(currentPlayer);
+            }
+            else if(gameSelection.equals("99")){
+                System.exit(0);
+            }
+        }
+    }
 
+    public void runBlackJackGame(Player currentPlayer){
+        System.out.print("Entering BlackJack\n\n");
+        //May cause issues return player to main screen
+        BlackJackPlayer blackJackPlayer = (BlackJackPlayer) currentPlayer;
+        BlackJack blackJack = new BlackJack();
+    }
 
+    public void runGoFish(Player currentPlayer){
+        System.out.print("Entering Go Fish\n\n");
+        //May cause issues return player to main screen
+        GoFishPlayer goFishPlayer = (GoFishPlayer) currentPlayer;
+        GoFishGame goFish = new GoFishGame();
+    }
+
+    public void runCraps(Player currentPlayer){
+        System.out.print("Entering Craps\n\n");
+        //May cause issues return player to main screen
+        CrapsPlayer crapsPlayer = (CrapsPlayer) currentPlayer;
+        CrapsGame craps = new CrapsGame();
+    }
+
+    public void runChoHan(Player currentPlayer){
+        System.out.print("Entering Cho Han\n\n");
+        //May cause issues return player to main screen
+        ChoHanPlayer choHanPlayer = (ChoHanPlayer) currentPlayer;
+        //ChoHan choHan = new ChoHan();
     }
 
 
--- src/main/java/io/zipcoder/casino/GameInterface/DiceGame/CrapsGame.java
+++ src/main/java/io/zipcoder/casino/GameInterface/DiceGame/CrapsGame.java
@@ -1,12 +1,7 @@
 package io.zipcoder.casino.GameInterface.DiceGame;
 import io.zipcoder.casino.GameInterface.GamblingGameInterface;
-
-<<<<<<< HEAD
-=======
-
 import io.zipcoder.casino.GameInterface.GamblingGameInterface;
 
->>>>>>> dc13be03049bdf8c64e6bd0cc9b41b68af0dd9cf
 public class CrapsGame extends DiceGame implements GamblingGameInterface {
     public CrapsGame(){
         super();
