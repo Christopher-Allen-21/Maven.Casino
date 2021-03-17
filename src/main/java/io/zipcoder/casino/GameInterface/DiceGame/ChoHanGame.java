22b92d9bce5db9d6f7da5142754ddf9227521f76
KellyPorter02 <porter.kelly02@gmail.com>
2021-03-17 16:49:55 -0400

    checkpointing: more methods for Cho-Han

--- src/main/java/io/zipcoder/casino/GameInterface/DiceGame/ChoHanGame.java
+++ src/main/java/io/zipcoder/casino/GameInterface/DiceGame/ChoHanGame.java
@@ -12,6 +12,11 @@ public class ChoHanGame implements GameInterface {
 
     private Player player1;
     private int rollValue;
+    private double playerBet;
+    private int playerChoice;  // 1 for odd, 2 for even
+    private boolean diceOddEven;  // true for odd, false for even
+
+    private Dice playerDice = new Dice();
 
     public ChoHanGame(Player player1) {
         this.player1 = player1;
@@ -40,46 +45,47 @@ public class ChoHanGame implements GameInterface {
 
         }
     */
-    private Dice playerDice = new Dice();
+
 
     public void greetPlayer() {
-        System.out.println("Welcome to Cho Han!");
+        System.out.println("Welcome to Cho-Han!");
     }
 
-    public void askEvenOdd() {
+    public boolean askEvenOdd() {
         System.out.println("Place your bet: Even or Odd?");
         String playerChoice = "";
+        // put in while loop
         playerChoice = myConsole.getStringInput("Select 1 for 'odd', Select 2 for 'even'");
         if (playerChoice.equals("1")) {
             // note odd - save to variable?
         } else if (playerChoice.equals("2")) {
-            // note even - save to variable?
+            // note even - save to variable? an int 1 or 2
         }
         else {
-            getStringInput("Please enter 1 or 2");
+            getStringInput("Please enter 1 or 2"); // testing if 1 or 2
         }
     }
 
     public void askWager() {
-        Double playerBet = myConsole.getDoubleInput("How much would you like to bet? Enter dollar amount.");
+        playerBet = myConsole.getDoubleInput("How much would you like to bet? Enter dollar amount.");
         player1.placeBet(); // askWager method same as placeBet method of Gambling player superclass? need to override?
+        //need to extend gambling player class to player
         //System.out.printf("%8.2f", x); - print in money format
         //System.out.printf("Hello, %s. Next year, you'll be %d", name, age); format with multiple args and multiple formats
         System.out.printf("You have wagered %8.2F", playerBet); // need to doublecheck formatting on this, maybe just use "You have wagered $" + playerBet + ".00."
     }
 
-    public int askRollDice() {
-        int rollValue;
+    public int askRollDice() { // needs tweaks
         System.out.println("Are you ready to roll?");
-        String playerChoice = myConsole.getStringInput("Enter 1 to roll dice");
-        if (playerChoice.equals("1")) {
-            rollValue = playerDice.rollPairDice(); // Display dice - make display dice method.
-            return rollValue;
-        }
-        else {
-            playerChoice = myConsole.getStringInput("You must enter 1 to roll the dice. You may enter 0 to exit the game and return to the game menu."); // figure this one out
-            // need loop here? to return to beginning of method?
-
+        while (true) {
+            String playerChoice = myConsole.getStringInput("Enter 1 to roll dice");
+            if (playerChoice.equals("1")) {
+                rollValue = playerDice.rollPairDice(); // Display dice - make display dice method.
+                return rollValue;
+            }
+            else {
+                playerChoice = myConsole.getStringInput("You must enter 1 to roll the dice.");
+            }
         }
     }
 
@@ -92,61 +98,41 @@ public class ChoHanGame implements GameInterface {
 
     public void getDie2Image() {} // ASCI art of the two dice
 
-    public boolean getOddEven () {
+    public boolean ifOdd() {
         if (rollValue % 2 != 0) {
             return true;          // true if roll odd
         } else {
             return false;         // false if roll even
         }
     }
+    public boolean getWinLoss() { // possibly use .checkWinner here from Game Interface
+        if (playerChoice == 1 && ifOdd()) {
+            System.out.println("Congratulations, you win!");
+            return true;
+        } else if (playerChoice == 2 && ifOdd()) {
+            System.out.println("Congratulations, you win!");
+            return true;
+        }
+        else {
+            System.out.println("Sorry, you lost! Better luck next time.");
+            return false;
 
-    public String getStringInput(String prompt, Object... args) {
-        println(prompt, args);
-        return input.nextLine();
-
-        /*
-        greet player
-
-        do you bet on even or odd?
-
-        .greet() {
-            System.out.println("Welcome to Cho Han!");
         }
-        .askEvenOrOdd(String playerChoice) {
-            String playerChoice = "";
-            playerChoice = myConsole.getStringInput("1 - 'Hit', 2 - 'Stay'");
-            System.out.println("Place your bet; even or odd?");
-            System.out.println("Select 1 for odd");
-            System.out.println("Select 2 for even");
-        }
-        .askBetAmount(Double playerBet) {
-            Double this.playerBet = playerBet;
-            System.out.println("How much would you like to bet?");
-        }
-        .setBetAmount() {
-            save user input
-        }
-        .askPlayerToRoll() {
-            System.out.println("Press 1 when ready to roll the dice!");
-        }
-        .rollPairDice() {
-            return total;
-        }
-        .asciDice() {
-            int valDie1 = .getDie1;
-            int valDie2 = .getDie2;
-            System.out.println("asci");
-        }
-        .getWinner() {
-        }
-        .askPlayAgain() {
-        }
-
-
-
-        */
-
     }
+    public int askPlayAgain() {
+        System.out.println("Would you like to play again?");
+        String playerChoice = myConsole.getStringInput("Enter 1 play again or 2 to exit to the Casino."); // should new variable get declared here? or can reuse playChoice.
+        if (playerChoice.equals("1")) {
+            // start loop of .startGame() over again
+        }
+        else if (playerChoice.equals("2")) {
+            //
+        }
+        else {
+        }   return -1;
+    }
+
+
 
     public void compareTotals () {
     }
