63f3a2224b6e5e5a8f0dec024c76f629cf3065c0
Chris Allen <Chris@zipcodes-MacBook-Pro-350.local>
2021-03-17 10:20:14 -0400

    Created AI choose card method

--- src/test/java/io/zipcoder/casino/CardGame/GoFishGameTest.java
+++ src/test/java/io/zipcoder/casino/CardGame/GoFishGameTest.java
@@ -5,6 +5,7 @@ import io.zipcoder.casino.Player.Player;
 import junit.framework.TestCase;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class GoFishGameTest extends TestCase {
 
@@ -39,5 +40,14 @@ public class GoFishGameTest extends TestCase {
         Player player1 = new Player("Bob");
         ArrayList<Card> hand = new ArrayList<>() ;
 
+    }
+
+
+    public void testAIAskForCards(){
+        Player player1 = new Player("Chris");
+        GoFishGame game = new GoFishGame(player1);
+        for(int i=0;i<1000;i++){
+            //System.out.println(game.aiAskForCards());
+        }
     }
 }
